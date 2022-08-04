133021a9f8c1dd1f25799e6b50726c0268304142
LostPigxx <simplelx@zju.edu.cn>
2022-08-04 10:24:15 +0800

    SIMPLIFY THREAD MODULE

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <atomic>
 #include <chrono>
+#include <vector>
 
 #include <hiredis/hiredis.h>
 
@@ -62,19 +63,25 @@ private:
     std::atomic_bool is_producer_done_;
 };
 
-using RedisServer = std::pair<std::string, int>;
+class RedisServer {
+public:
+    std::string ip_;
+    int port_;
+    std::string pwd_;
+};
 
 constexpr std::size_t MAX_LIST_LEN = 10000;
 
-void ScanThread(RedisServer& server, LockedList& list_1) {
-    redisContext* redis = redisConnect(server.first.c_str(), server.second);
+void ScanThread(RedisServer& src_svr, std::vector<LockedList>& lists) {
+    redisContext* redis = redisConnect(src_svr.ip_.c_str(), src_svr.port_);
     redisReply* r = nullptr;
 
-    r = (redisReply*)redisCommand(redis, "auth a");
+    r = (redisReply*)redisCommand(redis, "auth %b", src_svr.pwd_.data(), src_svr.pwd_.size());
     std::cout << r->str << std::endl;
     freeReplyObject(r);
 
-    int cnt = 0;
+    uint64_t cnt = 0;
+    int list_index = 0;
     std::string cursor = "0";
     while (true) {
         r = (redisReply*)redisCommand(redis, "scan %b count 100", cursor.data(), cursor.size());
@@ -83,7 +90,13 @@ void ScanThread(RedisServer& server, LockedList& list_1) {
         for (auto i = 0u; i < r->element[1]->elements; ++i) {
             RestoreItem item;
             item.key_ = std::string(r->element[1]->element[i]->str, r->element[1]->element[i]->len);
-            list_1.Push(std::move(item));
+
+            // 避免任务堆积过多导致OOM
+            while (lists[list_index].Size() > MAX_LIST_LEN) {
+                list_index = (list_index + 1) % lists.size();
+            }
+            lists[list_index].Push(std::move(item));
+
             ++cnt;
             if (cnt % 10000 == 0) {
                 std::cout << "Scan thread count: " << cnt << std::endl;
@@ -91,112 +104,86 @@ void ScanThread(RedisServer& server, LockedList& list_1) {
         }
         freeReplyObject(r);
 
-        if (list_1.Size() > MAX_LIST_LEN) {
-            std::this_thread::sleep_for(100ms);
-        }
-
         if (cursor == "0") {
             break;
         }
     }
 
-    std::cout << cnt << std::endl;
-    list_1.Done();
-
-    redisFree(redis);
-}
-
-void DumpAndTtlThead(RedisServer& server, LockedList& list_1, LockedList& list_2) {
-    redisContext* redis = redisConnect(server.first.c_str(), server.second);
-    redisReply* r = nullptr;
-
-    r = (redisReply*)redisCommand(redis, "auth a");
-    std::cout << r->str << std::endl;
-    freeReplyObject(r);
-
-    int cnt = 0;
-    while (!list_1.IsProducerDone() || list_1.Size() > 0) {
-        auto item = list_1.Pop();
-        if (item.Empty()) {
-            std::this_thread::sleep_for(1s);
-            continue;
-        }
-
-        // dump
-        r = (redisReply*)redisCommand(redis, "dump %b", item.key_.data(), item.key_.size());
-        item.val_ = std::string(r->str, r->len);
-        freeReplyObject(r);
-        // ttl
-        r = (redisReply*)redisCommand(redis, "ttl %b", item.key_.data(), item.key_.size());
-        if (r->integer < 0) {
-            item.ttl_ = 0;  // restore命令中ttl为0表示不过期
-        } else {
-            item.ttl_ = r->integer;
-        }
-        freeReplyObject(r);
-
-        list_2.Push(std::move(item));
-        if (list_2.Size() > MAX_LIST_LEN) {
-            std::this_thread::sleep_for(100ms);
-        }
-
-        ++cnt;
-        if (cnt % 10000 == 0) {
-            std::cout << "Dump and ttl thread count: " << cnt << std::endl;
-        }
-    }
-
-    list_2.Done();
-    redisFree(redis);
-}
-
-void RestoreThread(RedisServer& server, LockedList& list_2) {
-    redisContext* redis = redisConnect(server.first.c_str(), server.second);
-    redisReply* r = nullptr;
-
-    r = (redisReply*)redisCommand(redis, "auth a");
-    std::cout << r->str << std::endl;
-    freeReplyObject(r);
-
-    int cnt = 0;
-    while (!list_2.IsProducerDone() || list_2.Size() > 0) {
-        auto item = list_2.Pop();
-        if (item.Empty()) {
-            std::this_thread::sleep_for(1s);
-            continue;
-        }
-
-        r = (redisReply*)redisCommand(redis, "restore %b %llu %b REPLACE",
-            item.key_.data(), item.key_.size(), item.ttl_, item.val_.data(), item.val_.size());
-        freeReplyObject(r);
-
-        ++cnt;
-        if (cnt % 10000 == 0) {
-            std::cout << "Restore thread count: " << cnt << std::endl;
-        }
+    for (auto& list : lists) {
+        list.Done();
     }
 
     redisFree(redis);
 }
 
-//
-// 1 scan thread
-// N dump&ttl thread
-// M restore thread
+void WorkerThread(RedisServer& src_svr, RedisServer& dst_svr, std::vector<LockedList>& lists) {
+    std::vector<std::thread> threads;
+    for (auto i = 0u; i < lists.size(); ++i) {
+        threads.emplace_back([&](std::size_t id, LockedList& list) {
+            redisContext* src_redis = redisConnect(src_svr.ip_.c_str(), src_svr.port_);
+            redisContext* dst_redis = redisConnect(dst_svr.ip_.c_str(), dst_svr.port_);
+            redisReply* r = nullptr;
+
+            r = (redisReply*)redisCommand(src_redis, "auth %b", src_svr.pwd_.data(), src_svr.pwd_.size());
+            freeReplyObject(r);
+            r = (redisReply*)redisCommand(dst_redis, "auth %b", dst_svr.pwd_.data(), dst_svr.pwd_.size());
+            freeReplyObject(r);
+
+            uint64_t cnt = 0;
+            while (!list.IsProducerDone() || list.Size() > 0) {
+                auto item = list.Pop();
+                if (item.Empty()) {
+                    std::this_thread::sleep_for(100ms);
+                    continue;
+                }
+
+                // dump
+                r = (redisReply*)redisCommand(src_redis, "dump %b", item.key_.data(), item.key_.size());
+                item.val_ = std::string(r->str, r->len);
+                freeReplyObject(r);
+
+                // ttl
+                r = (redisReply*)redisCommand(src_redis, "ttl %b", item.key_.data(), item.key_.size());
+                if (r->integer < 0) {
+                    item.ttl_ = 0;  // restore命令中ttl为0表示不过期
+                } else {
+                    item.ttl_ = r->integer;
+                }
+                freeReplyObject(r);
+
+                // restore
+                r = (redisReply*)redisCommand(dst_redis, "restore %b %llu %b REPLACE",
+                        item.key_.data(), item.key_.size(), item.ttl_, item.val_.data(), item.val_.size());
+                freeReplyObject(r);
+
+                ++cnt;
+                if (cnt % 10000 == 0) {
+                    std::cout << "WorkerThread " << id << " count: " << cnt << std::endl;
+                }
+            }
+
+            redisFree(src_redis);
+            redisFree(dst_redis);
+        }, i, std::ref(lists[i]));
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+}
+
+
 int main(int argc, char** argv) {
     // TODO: 通过输入获取
-    RedisServer src_redis = {"127.0.0.1", 6379};
-    RedisServer dest_redis = {"127.0.0.1", 6380};
+    RedisServer src_redis = {"127.0.0.1", 6379, "a"};
+    RedisServer dst_redis = {"127.0.0.1", 6380, "a"};
 
-    LockedList list_1;  // scan_thread -> dump_ttl_thread
-    LockedList list_2;  // dump_ttl_thread -> restore_thread
-    std::thread scan_thread(ScanThread, std::ref(src_redis), std::ref(list_1));
-    std::this_thread::sleep_for(1s);
-    std::thread dump_ttl_thread(DumpAndTtlThead, std::ref(src_redis), std::ref(list_1), std::ref(list_2));
-    std::this_thread::sleep_for(1s);
-    std::thread restore_thread(RestoreThread, std::ref(dest_redis), std::ref(list_2));
+    int N = 16;
+    std::vector<LockedList> lists(N);
+
+    std::thread scan_thread(ScanThread, std::ref(src_redis), std::ref(lists));
+    std::thread worker_thread(WorkerThread, std::ref(src_redis), std::ref(dst_redis), std::ref(lists));
 
     scan_thread.join();
-    dump_ttl_thread.join();
-    restore_thread.join();
+    worker_thread.join();
 }
